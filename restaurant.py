5199124ae5e029c7298884c93dce4d6959842684
Inma Gijón <igijoninf@gmail.com>
2022-11-12 12:39:26 +0100

    Checkbox

--- restaurant.py
+++ restaurant.py
@@ -2,20 +2,43 @@ from tkinter import *
 
 
 def init():
+
+    # crear aplicación
+    aplication = new_application()
+
+    # crear paneles
+    panel_superior, panel_izquierdo, panel_coste, panel_comidas, panel_bebidas, panel_postres, panel_derecha, \
+    panel_calculadora, panel_recibo, panel_botones = new_panels(aplication)
+
+    # lista de productos
+    lista_comidas = ['pollo', 'cordero', 'salmón', 'merzlua', 'kebab', 'pizza1', 'pizza2', 'pizza3']
+    lista_bebidas = ['agua', 'fanta', 'coca cola', 'vino', 'cerveza', 'zumo', 'tónica', 'bitter']
+    lista_postres = ['helado', 'brownie', 'fruta', 'flan', 'mousse', 'pastel', 'tarta zanahoria', 'tarta de chocolate']
+
+    gen_items(lista_comidas, panel_comidas)
+    gen_items(lista_bebidas, panel_bebidas)
+    gen_items(lista_postres, panel_postres)
+
+    # Evitar que la ventana se cierre
+    aplication.mainloop()
+
+def new_application():
     # Iniciar tkinter
     aplication = Tk()
     # tamaño de la ventana
     aplication.geometry('1020x630+0+0')
 
     # evitar maximizar
-    aplication.resizable(0, 0) # no se puede modificar en ningún eje
+    aplication.resizable(0, 0)  # no se puede modificar en ningún eje
 
     # Título de la ventana
     aplication.title('Mi restaurante - Sistema de facturación')
 
     # Color de fondo de la ventana. También permite RGB
     aplication.config(bg='burlywood')
+    return aplication
 
+def new_panels(aplication: object) -> object:
     # panel superior
     panel_superior = Frame(aplication, bd=1, relief=FLAT)
     panel_superior.pack(side=TOP)
@@ -25,7 +48,7 @@ def init():
 
     # panel izquierdo
     panel_izquierdo = Frame(aplication, bd=1, relief=FLAT)
-    panel_superior.pack(side=LEFT)
+    panel_izquierdo.pack(side=LEFT)
 
     # panel coste
     panel_coste = Frame(panel_izquierdo, bd=1, relief=FLAT)
@@ -33,7 +56,7 @@ def init():
 
     # panel de menú
     panel_comidas = LabelFrame(panel_izquierdo, text='Comida', font=('Dosis', 19, 'bold'),
-                            bd=1, relief=FLAT, fg='azure4')
+                               bd=1, relief=FLAT, fg='azure4')
     panel_comidas.pack(side=LEFT)
     panel_bebidas = LabelFrame(panel_izquierdo, text='Bebidas', font=('Dosis', 19, 'bold'),
                                bd=1, relief=FLAT, fg='azure4')
@@ -42,16 +65,29 @@ def init():
                                bd=1, relief=FLAT, fg='azure4')
     panel_postres.pack(side=LEFT)
 
-    #panel derecha
+    # panel derecha
     panel_derecha = Frame(aplication, bd=1, relief=FLAT)
     panel_derecha.pack(side=RIGHT)
 
     panel_calculadora = Frame(panel_derecha, bd=1, relief=FLAT, bg='burlywood')
-    panel_calculadora.pack(side=TOP)
+    panel_calculadora.pack()
     panel_recibo = Frame(panel_derecha, bd=1, relief=FLAT, bg='burlywood')
-    panel_recibo.pack(side=TOP)
+    panel_recibo.pack()
     panel_botones = Frame(panel_derecha, bd=1, relief=FLAT, bg='burlywood')
-    panel_botones.pack(side=TOP)
+    panel_botones.pack()
 
-    # Evitar que la ventana se cierre
-    aplication.mainloop()
+    return (panel_superior, panel_izquierdo, panel_coste, panel_comidas, panel_bebidas, panel_postres, panel_derecha, \
+    panel_calculadora, panel_recibo, panel_botones)
+
+
+def gen_items(items_list, panel):
+    # Generar items comida
+    variable = []
+    cont = 0
+    for comida in items_list:
+        variable.append(IntVar())  # Crea una variable entera propia de Tkinter
+        boton = Checkbutton(panel, text=comida.title(), font=('Dosis', 19, 'bold'),
+                            onvalue=1, offvalue=0, variable=variable[cont])
+        boton.grid(row=cont, column=1, sticky=W)  # Se colocan a la izquierda
+        cont += 1
+
