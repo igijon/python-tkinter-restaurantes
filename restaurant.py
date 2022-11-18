3d8252263ee90f77b8f00ae1ff3d81fbed0ab897
Inma Gijón <igijoninf@gmail.com>
2022-11-18 19:43:49 +0100

    Calcular totales

--- restaurant.py
+++ restaurant.py
@@ -1,11 +1,21 @@
 from tkinter import *
 
 operador = ''
+precios_comida = [1.32, 1.65, 2.31, 3.22, 1.22, 1.99, 2.05, 2.65]
+precios_bebida = [0.25, 0.99, 1.21, 1.54, 1.08, 1.10, 2.00, 1.58]
+precios_postres = [1.54, 1.68, 1.32, 1.97, 2.55, 2.14, 1.94, 1.74]
+text_comidas = []
+text_bebidas = []
+text_postres = []
+var_precio_comida = ''
+var_precio_bebidas = ''
+var_precio_postres = ''
+var_precio_subtotal = ''
+var_precio_impuestos = ''
+var_precio_total = ''
+text = {}
 
 def init():
-
-
-
     # crear aplicación
     aplication = new_application()
 
@@ -18,16 +28,22 @@ def init():
     lista_bebidas = ['agua', 'fanta', 'coca cola', 'vino', 'cerveza', 'zumo', 'tónica', 'bitter']
     lista_postres = ['helado', 'brownie', 'fruta', 'flan', 'mousse', 'pastel', 'tarta zanahoria', 'tarta de chocolate']
 
-    gen_items(lista_comidas, panel_comidas)
-    gen_items(lista_bebidas, panel_bebidas)
-    gen_items(lista_postres, panel_postres)
+    gen_items(lista_comidas, panel_comidas, text_comidas)
+    gen_items(lista_bebidas, panel_bebidas, text_bebidas)
+    gen_items(lista_postres, panel_postres, text_postres)
 
+    gen_precios(panel_coste, 'Total', 2, 2)
     gen_precios(panel_coste, 'Comida', 0, 0)
     gen_precios(panel_coste, 'Bebida', 1, 0)
     gen_precios(panel_coste, 'Postre', 2, 0)
     gen_precios(panel_coste, 'Subtotal', 0, 2)
     gen_precios(panel_coste, 'Impuestos', 1, 2)
-    gen_precios(panel_coste, 'Total', 2, 2)
+    text['Total'].config(textvariable=var_precio_total)
+    text['Comida'].config(textvariable=var_precio_comida)
+    text['Bebida'].config(textvariable=var_precio_bebidas)
+    text['Postre'].config(textvariable=var_precio_postres)
+    text['Subtotal'].config(textvariable=var_precio_subtotal)
+    text['Impuestos'].config(textvariable=var_precio_impuestos)
 
     gen_buttons(panel_botones)
 
@@ -37,6 +53,7 @@ def init():
 
     # Evitar que la ventana se cierre
     aplication.mainloop()
+
 
 def new_application():
     # Iniciar tkinter
@@ -53,6 +70,7 @@ def new_application():
     # Color de fondo de la ventana. También permite RGB
     aplication.config(bg='burlywood')
     return aplication
+
 
 def new_panels(aplication: object) -> object:
     # panel superior
@@ -97,11 +115,12 @@ def new_panels(aplication: object) -> object:
     panel_calculadora, panel_recibo, panel_botones)
 
 
-def gen_items(items_list, panel):
+
+def gen_items(items_list, panel, text):
     # Generar items comida
     variable = []
-    entrada=[]
-    text=[]
+    entrada = []
+
     cont = 0
     for comida in items_list:
         # Crear checkbuttons
@@ -131,7 +150,20 @@ def gen_items(items_list, panel):
 
 
 def gen_precios(panel, concepto, row, column):
-    var_precio = StringVar()
+    global var_precio_comida
+    global var_precio_bebidas
+    global var_precio_postres
+    global var_precio_total
+    global var_precio_impuestos
+    global var_precio_subtotal
+    global text
+
+    var_precio_comida = StringVar()
+    var_precio_bebidas = StringVar()
+    var_precio_postres = StringVar()
+    var_precio_subtotal = StringVar()
+    var_precio_impuestos = StringVar()
+    var_precio_total = StringVar()
 
     label_precio = Label(panel,
                          text='Precio '+concepto,
@@ -139,16 +171,17 @@ def gen_precios(panel, concepto, row, column):
                          bg='azure4',
                          fg='white')
     label_precio.grid(row=row, column=column)
-    text = Entry(panel,
-                 font=('Dosis', 12, 'bold'),
-                 bd=1,
-                 width=10,
-                 state='readonly',
-                 textvariable=var_precio)
-    text.grid(row=row, column=column+1, padx=41)
+    text[concepto]=(Entry(panel,
+                    font=('Dosis', 12, 'bold'),
+                    bd=1,
+                    width=10,
+                    state='readonly'))
+    text[concepto].grid(row=row, column=column+1, padx=41)
+
 
 def gen_buttons(panel):
     buttons = ['total', 'recibo', 'guardar', 'reset']
+    botones_creados = []
     column = 0
     for button in buttons:
         b = Button(panel,
@@ -161,7 +194,10 @@ def gen_buttons(panel):
                    height=2)
         b.grid(row=0,
                column=column)
+        botones_creados.append(b)
+        click_botones(len(botones_creados)-1, botones_creados, buttons)
         column += 1
+
 
 def gen_recibo(panel):
     texto_recibo = Text(panel,
@@ -171,6 +207,7 @@ def gen_recibo(panel):
                         height=10)
     texto_recibo.grid(row=0,
                       column=0)
+
 
 def gen_calculadora(panel):
     visor_calculadora = Entry(panel,
@@ -192,8 +229,8 @@ def gen_calculadora(panel):
                                  font=('Dosis', 16, 'bold'),
                                  fg='black',
                                  bg='azure4',
-                                   bd=1,
-                                   width=5))
+                                 bd=1,
+                                 width=5))
 
         cont = len(botones_guardados)-1
         botones_guardados[cont].grid(row=row,
@@ -215,6 +252,7 @@ def click_calculadora(num, botones_calculadora, botones_guardados, visor_calcula
         botones_guardados[num].config(command=lambda: borrar(visor_calculadora))
     else:
         botones_guardados[num].config(command=lambda: get_result(visor_calculadora))
+
 
 def click_boton(num, visor_calculadora):
     global operador
@@ -249,3 +287,50 @@ def revisar_check(variable, entrada, text):
             e.config(state=DISABLED)
             text[x].set('0')
         x += 1
+
+
+def click_botones(num, botones_creados, botones_etiquetas):
+    if botones_etiquetas[num] == 'total':
+        botones_creados[num].config(command=lambda: total())
+
+
+def total_parcial(var, text, precios):
+    sub_total = 0
+
+    for cont in range(len(precios)):
+        try:
+            cant = float(text[cont].get())
+        except ValueError:
+            cant = 0
+        sub_total += cant*precios[cont]
+    var.set(f'{round(sub_total, 2)} €')
+    return sub_total
+
+
+def total():
+    global var_precio_comida
+    global var_precio_bebidas
+    global var_precio_postres
+    global var_precio_subtotal
+    global var_precio_impuestos
+    global var_precio_total
+
+    global text_comidas
+    global text_bebidas
+    global text_postres
+
+    sub_total_comida = total_parcial(var_precio_comida, text_comidas, precios_comida)
+    var_precio_comida.set(f'{round(sub_total_comida, 2)} €')
+
+    sub_total_bebida = total_parcial(var_precio_bebidas, text_bebidas, precios_bebida)
+    var_precio_bebidas.set(f'{round(sub_total_bebida, 2)} €')
+
+    sub_total_postres = total_parcial(var_precio_postres, text_postres, precios_postres)
+    var_precio_postres.set(f'{round(sub_total_postres, 2)} €')
+
+    sub_total = sub_total_comida+sub_total_bebida+sub_total_postres
+    impuestos = sub_total * 0.21
+    precio_total = sub_total+impuestos
+    var_precio_total.set(f'{round(precio_total, 2)} €')
+    var_precio_subtotal.set(f'{round(sub_total, 2)} €')
+    var_precio_impuestos.set(f'{round(impuestos, 2)} €')
